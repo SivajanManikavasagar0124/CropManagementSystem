7fa4ec76205f9ab17cb0b63588355494ed485336
SivajanM <sivajanm@gmail.com>
2021-10-12 14:18:14 -0400

    adding settings fragment into code, moved metric or imperial units to settings from fragment_croptemperature.xml, added basic textview, spinner, and switch with no functionality.

--- SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/SunshineMain.java
+++ SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/SunshineMain.java
@@ -17,8 +17,6 @@ import android.view.View;
 import android.view.Menu;
 import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
@@ -68,6 +66,7 @@ public class SunshineMain extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
 
@@ -77,8 +76,8 @@ public class SunshineMain extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.action_settings:
                 Toast.makeText(this, R.string.settingsfragmentopen, Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(SunshineMain.this, SettingActivity.class);
-                startActivity(i);
+                SettingActivity settingActivity = new SettingActivity();
+                SunshineMain.this.getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, settingActivity).addToBackStack(null).commit();
                 return true;
             case R.id.bugreport_settings:
                 Toast.makeText(this, R.string.placeholder, Toast.LENGTH_SHORT).show();
