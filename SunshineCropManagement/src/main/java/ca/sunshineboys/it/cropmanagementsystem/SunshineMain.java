53b7a1aa6dd053cf10655293135a7599f4f329bb
muhammadQamar4609 <78670538+muhammadQamar4609@users.noreply.github.com>
2021-11-14 19:51:19 -0500

    Removed unnecessary comments from the main file

--- SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/SunshineMain.java
+++ SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/SunshineMain.java
@@ -33,14 +33,7 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-/*
-Sivajan Manikavasagar (Team Leader) N01240148
-Muhammad Qamar N01344609
-Noha Philips N01351336
-Tanvir Pahwa N01245843
-CENG 322 - RNC/D
-CENG 317 - 0NF
- */
+
 public class SunshineMain extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
