ad549a46d75d2524797647cc3828bee4546ab1fb
tanvirPahwa5843 <N01245843@humbermail.ca>
2021-11-14 21:33:32 -0500

    added popup window for the bug report

---
+++ SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/PopUp.java
@@ -0,0 +1,15 @@
+package ca.sunshineboys.it.cropmanagementsystem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PopUp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.pop_up);
+
+    }
+}
