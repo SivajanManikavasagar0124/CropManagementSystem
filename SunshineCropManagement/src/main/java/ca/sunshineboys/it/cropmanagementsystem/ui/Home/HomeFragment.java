743b13d25dcbfc1024df1690600fb87430614f9b
SivajanM <sivajanm@gmail.com>
2021-11-30 15:51:17 -0500

    UI/UX Changes, small modification with colours.

--- SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/ui/Home/HomeFragment.java
+++ SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/ui/Home/HomeFragment.java
@@ -33,13 +33,12 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_homescreen);
-        TextView homescreenTemp = root.findViewById(R.id.HomeScreenTemp);
+
 
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
         return root;
