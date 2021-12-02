2ee4e06ee5380d3ac34b49f4b26c4f24b5b83750
SivajanM <sivajanm@gmail.com>
2021-12-02 07:43:55 -0500

    added datavalues for sensorConnected and added dynamic text and picture to indicate if sensors are connected.

--- SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/ui/Home/HomeFragment.java
+++ SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/ui/Home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -26,21 +27,46 @@ import ca.sunshineboys.it.cropmanagementsystem.ui.SoilMoisture.MoistureViewModel
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    ImageView sensorPicture;
+    TextView sensorText;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
+
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        //Intialise OBJECTIDS
+        sensorPicture = root.findViewById(R.id.connectionImage);
+        sensorText = root.findViewById(R.id.connectionText);
 
 
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        DatabaseReference sensorRef = database.getReference("sensorConnected");
+        sensorRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+            if (snapshot.exists()){
+                sensorPicture.setImageResource(R.drawable.greenconnection);
+                sensorText.setText("Connected");
+            }else{ //If snapshot == NULL show disconnected
+                sensorPicture.setImageResource(R.drawable.redconnection);
+                sensorText.setText("Disconnected");
+            }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        /*homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
 
             }
-        });
+        });*/
         return root;
     }
 }
