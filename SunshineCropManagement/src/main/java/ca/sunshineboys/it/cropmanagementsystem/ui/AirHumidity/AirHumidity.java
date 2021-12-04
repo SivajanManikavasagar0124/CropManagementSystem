03b6cadc8ad0cfc7bbf8d7880ff505e36a6ba44c
tanvirPahwa5843 <N01245843@humbermail.ca>
2021-12-04 18:03:28 -0500

    unsure as to why i cannot get information from the database after going through the doc
    committing this with the code from the doc as commented so teammates can view it and give their opinions
    
    added Post.java as recommended in the doc

--- SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/ui/AirHumidity/AirHumidity.java
+++ SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/ui/AirHumidity/AirHumidity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -17,6 +18,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import ca.sunshineboys.it.cropmanagementsystem.Post;
 import ca.sunshineboys.it.cropmanagementsystem.R;
 /*
 Sivajan Manikavasagar (Team Leader) N01240148
@@ -28,20 +39,52 @@ CENG 317 - 0NF
  */
 public class AirHumidity extends Fragment {
 
+ //   private DatabaseReference databaseReference;
+
     private HumidityViewModel homeViewModel;
+
+    Button humidityMeasure;
+    TextView humidityText;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HumidityViewModel.class);
         View root = inflater.inflate(R.layout.fragment_airhumidity, container, false);
-       //final TextView textView = root.findViewById(R.id.text_airhumidityHumid);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+     //   databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        humidityMeasure = root.findViewById(R.id.airHumidityButton);
+        humidityText = root.findViewById(R.id.humidityLevelText);
+
+        Snackbar sensorError = Snackbar.make(getActivity().findViewById(android.R.id.content), "Unable to update sensor please try again.",Snackbar.LENGTH_SHORT);
+        Snackbar sensorUpdating = Snackbar.make(getActivity().findViewById(android.R.id.content), "Currently updating data from the sensor.",Snackbar.LENGTH_SHORT);
+
+        humidityMeasure.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-               // textView.setText(s);
+            public void onClick(View v) {
+                /*
+                ValueEventListener postListener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                      //  Post post = snapshot.getValue(Post.class);
+                        int humidity = snapshot.child("temperatureHumidity").getValue(Integer.class);
+                        humidityText.setText(humidity);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                };
+               //mPostReference.addValueEventListener(postListener);
+               */
+
             }
         });
+
         return root;
     }
+
+
 }
