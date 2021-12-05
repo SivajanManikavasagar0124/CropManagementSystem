93c715c4cdfdae410cacd0b87a0792940226f4d6
tanvirPahwa5843 <N01245843@humbermail.ca>
2021-12-04 19:50:28 -0500

    added functionality on the crop temperature screen
    data is now pulled from the database

--- SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/ui/CropTemperature/CropTemperature.java
+++ SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/ui/CropTemperature/CropTemperature.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 import ca.sunshineboys.it.cropmanagementsystem.R;
 /*
@@ -23,13 +32,35 @@ CENG 317 - 0NF
  */
 public class CropTemperature extends Fragment {
 
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
     private TemperatureViewModel galleryViewModel;
+
+    TextView tempText;
+    Button tempButt;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 new ViewModelProvider(this).get(TemperatureViewModel.class);
         View root = inflater.inflate(R.layout.fragment_croptemperature, container, false);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("absoluteTemperature");
+
+        tempButt = root.findViewById(R.id.NohaButton);
+        tempText = root.findViewById(R.id.tempLevelText);
+
+        getData();
+
+        tempButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData();
+            }
+        });
+
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -37,4 +68,20 @@ public class CropTemperature extends Fragment {
         });
         return root;
     }
+
+    private void getData() {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                tempText.setText(value + getString(R.string.c));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
