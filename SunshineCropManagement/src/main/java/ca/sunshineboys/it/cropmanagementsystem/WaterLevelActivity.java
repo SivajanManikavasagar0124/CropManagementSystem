764c00a58b9b7447ffadc8b5cd5c38531c9bd6f8
tanvirPahwa5843 <N01245843@humbermail.ca>
2021-12-04 19:23:59 -0500

    made it so the water level sensor is able to update in realtime using info from the database

--- SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/WaterLevelActivity.java
+++ SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/WaterLevelActivity.java
@@ -11,6 +11,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 //import com.gelitenight.waveview.library.WaveView;
 
@@ -24,7 +33,12 @@ CENG 317 - 0NF
  */
 public class WaterLevelActivity extends Fragment {
 
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
     private WaterLevelViewModel mViewModel;
+
+    TextView waterText;
   //  WaveView waveView;
     public static WaterLevelActivity newInstance() {
         return new WaterLevelActivity();
@@ -34,6 +48,13 @@ public class WaterLevelActivity extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
       View root = inflater.inflate(R.layout.water_level_fragment, container, false);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("waterDistance");
+
+        waterText = root.findViewById(R.id.waterlevelPercent);
+
+        getData();
             return root;
     }
 
@@ -42,6 +63,21 @@ public class WaterLevelActivity extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(WaterLevelViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private void getData() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                waterText.setText(value + getString(R.string.percent));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 }
