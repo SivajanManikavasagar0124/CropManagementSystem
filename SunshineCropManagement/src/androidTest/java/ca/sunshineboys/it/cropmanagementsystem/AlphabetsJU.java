1543165927b4a8ecea5fb175deaa8e613aa1ee04
Noha <nohageorgephilips@gmail.com>
2021-12-07 18:21:32 -0500

    16 first junit testing alphabets

---
+++ SunshineCropManagement/src/androidTest/java/ca/sunshineboys/it/cropmanagementsystem/AlphabetsJU.java
@@ -0,0 +1,23 @@
+package ca.sunshineboys.it.cropmanagementsystem;
+
+public class AlphabetsJU
+{
+    public String character(char a)
+    {
+      String result;
+      if( (a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+      result =  "ALPHABETS CLEAR";
+      else
+      result = " ALPHABETS CLEAR";
+      return result;
+    }
+     public static void main (String[] args)
+     {
+            AlphabetsJU test = new AlphabetsJU();
+            char a = '*';
+            String text = test.character(a);
+            System.out.println(a + text);
+     }
+    }
+
+
