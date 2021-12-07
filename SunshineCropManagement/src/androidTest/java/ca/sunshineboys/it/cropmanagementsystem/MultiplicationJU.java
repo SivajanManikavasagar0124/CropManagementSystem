0e6cbb4884b98408fb1302bb0d166848a4e9aa31
Noha <nohageorgephilips@gmail.com>
2021-12-07 18:53:15 -0500

    17 first junit testing multiplication

---
+++ SunshineCropManagement/src/androidTest/java/ca/sunshineboys/it/cropmanagementsystem/MultiplicationJU.java
@@ -0,0 +1,18 @@
+package ca.sunshineboys.it.cropmanagementsystem;
+
+public class MultiplicationJU
+{
+ public static void main(String[] args)
+ {
+  int num1 = 20;
+  int num2 = 20;
+  MultiplicationJU test = new MultiplicationJU();
+  int product = test.nums(num1,num2);
+  System.out.println(num1 + " x " + num2 + " = " + product);
+ }
+ public int nums(int num1, int num2)
+ {
+  int product = num1 * num2;
+  return product;
+ }
+}
