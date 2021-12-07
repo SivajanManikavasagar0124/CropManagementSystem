de7f3152cf3edfe771adec195e759ff40b33046d
Noha <nohageorgephilips@gmail.com>
2021-12-07 07:36:31 -0500

    10 edited pop up menu, implemented onclick listener and code for fab

--- SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/TaskSchedulerActivity.java
+++ SunshineCropManagement/src/main/java/ca/sunshineboys/it/cropmanagementsystem/TaskSchedulerActivity.java
@@ -1,7 +1,11 @@
 package ca.sunshineboys.it.cropmanagementsystem;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +15,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 /*
 Sivajan Manikavasagar (Team Leader) N01240148
 Muhammad Qamar N01344609
@@ -21,7 +29,9 @@ CENG 317 - 0NF
  */
 public class TaskSchedulerActivity extends Fragment {
 
+
     private TaskSchedulerViewModel mViewModel;
+
 
     public static TaskSchedulerActivity newInstance() {
         return new TaskSchedulerActivity();
@@ -30,7 +40,16 @@ public class TaskSchedulerActivity extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.task_scheduler_fragment, container, false);
+        View view = inflater.inflate(R.layout.task_scheduler_fragment, container, false);
+        CalendarView CV = (CalendarView) view.findViewById(R.id.calendarView);
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent I = new Intent(getActivity(), TSpopup.class);
+            }
+        });
+        return view;
     }
 
     @Override
