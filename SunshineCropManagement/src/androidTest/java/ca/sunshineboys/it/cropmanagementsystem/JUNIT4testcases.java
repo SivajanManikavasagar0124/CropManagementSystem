03dcc0afe2f48d092229a1a74599905287b03233
Noha <nohageorgephilips@gmail.com>
2021-12-07 20:14:25 -0500

    21 junit4 class file with all tests

---
+++ SunshineCropManagement/src/androidTest/java/ca/sunshineboys/it/cropmanagementsystem/JUNIT4testcases.java
@@ -0,0 +1,47 @@
+package ca.sunshineboys.it.cropmanagementsystem;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class JUNIT4testcases
+{
+ @Test
+ public void Addition()
+ {
+ AdditionJU t1 = new AdditionJU();
+ int actual = t1.AddAll(10);
+ int expected = 55;
+ assertEquals(expected,actual);
+ }
+ @Test
+ public void Multiplication()
+ {
+  MultiplicationJU t2 = new MultiplicationJU();
+  int actual = t2.nums(20, 20);
+  int expected = 400;
+  assertEquals(expected,actual);
+ }
+ @Test
+ public void PrimeN()
+ {
+  PrimeJU t3 = new PrimeJU();
+  String actual = t3.Prime(7);
+  String expected = "PRIME";
+  assertEquals(expected,actual);
+ }
+ @Test
+ public void Alphabet()
+ {
+  AlphabetsJU t4 = new AlphabetsJU();
+  String actual = t4.character('a');
+  String expected = "ALPHABETS CLEAR";
+  assertEquals(expected,actual);
+ }
+ @Test
+ public void Flip()
+ {
+  FlipJU t5 = new FlipJU ();
+  String actual = t5.Flip("hanoj");
+  String expected = "jonah";
+  assertEquals(expected,actual);
+ }
+}
