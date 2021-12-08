acfaea040cdfe4377a0b68c3d5dd8c558c10e534
Noha <nohageorgephilips@gmail.com>
2021-12-07 19:24:52 -0500

    19 first junit testing addition of numbers from 0 to 10

---
+++ SunshineCropManagement/src/androidTest/java/ca/sunshineboys/it/cropmanagementsystem/AdditionJU.java
@@ -0,0 +1,19 @@
+package ca.sunshineboys.it.cropmanagementsystem;
+
+public class AdditionJU
+{
+ public static void main(String[] args)
+ {
+  AdditionJU  test = new AdditionJU ();
+  int sum = test.AddAll(10);
+  System.out.println(" 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = " + sum);
+ }
+ public int AddAll(int num)
+ {
+  int added = 0;
+  for (int a = 0; a<= num; a++)
+  {
+   num += a;
+  }
+  return added;
+ }}
